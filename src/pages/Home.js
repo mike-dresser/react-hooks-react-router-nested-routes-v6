ff43138b2a5ab23ab4e4cfb83c286aa999d4aa4c
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-03 09:49:11 -0500

    completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,22 @@
-import { useState, useEffect } from "react";
-import UserCard from "../components/UserCard";
-import NavBar from "../components/NavBar";
+import UserCard from '../components/UserCard';
+import { Outlet, useOutletContext } from 'react-router-dom';
 
 function Home() {
-  const [users, setUsers] = useState([]);
+  const users = useOutletContext();
 
-  useEffect(() =>{
-    fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error));
-  }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>;
+  const userList = users.map((user) => {
+    return <UserCard key={user.id} user={user} />;
   });
 
   return (
     <>
-      <header>
-        <NavBar />
-      </header>
       <main>
         <h1>Home!</h1>
+        <Outlet context={users} />
         {userList}
       </main>
     </>
   );
-};
+}
 
 export default Home;
--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,36 @@
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Login from "./pages/Login";
-import UserProfile from "./pages/UserProfile";
-import ErrorPage from "./pages/ErrorPage";
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import UserProfile from './pages/UserProfile';
+import ErrorPage from './pages/ErrorPage';
+import App from './App';
 
 const routes = [
   {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />
-  }, 
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <ErrorPage />
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+        children: [
+          {
+            path: '/profile/:id',
+            element: <UserProfile />,
+          },
+        ],
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+    ],
   },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/profile/:id",
-    element: <UserProfile />,
-    errorElement: <ErrorPage />
-  }
 ];
 
 export default routes;
